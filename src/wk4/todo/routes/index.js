da34e37ab747ae5224e2c7c1fe6d8c434ea2c8e5
Shawn Cicoria <shawn@cicoria.com>
2015-10-02 14:20:28 -0400

    starting point

---
+++ src/wk4/todo/routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express - for CS612 Fall 2015' });
+});
+
+module.exports = router;
